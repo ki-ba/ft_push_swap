f37e5cec778eb046e2ebbab60eba69b93de28c64
ki-ba <barru.killian@pm.me>
2025-02-02 12:50:41 +0100

    norm

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: kbarru <kbarru@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 17:19:36 by kbarru            #+#    #+#             */
-/*   Updated: 2025/02/02 12:41:11 by kbarru           ###   ########lyon.fr   */
+/*   Updated: 2025/02/02 12:48:22 by kbarru           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,9 @@ int	main(int argc, char **argv)
 		input = ft_split((const char *)argv[1], ' ');
 	check_input(input);
 	while (input[i] && input[i] != argv[0])
-	{
-		ft_add_to_stack(&stack_a, input[i]);
-		++i;
-	}
-	// ft_print_stack(&stack_a);
+		ft_add_to_stack(&stack_a, input[i++]);
 	ft_stack_iter(&stack_a, ft_simplify_node);
 	success = ft_radix_sort(&stack_a, &stack_b);
-	// ft_print_stack(&stack_a);
 	ft_clearstack(&stack_a);
 	ft_clearstack(&stack_b);
 	return (!success);
